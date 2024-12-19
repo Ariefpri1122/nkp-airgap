4baf5b6c9ef31edf9cf637aa6255f66ccad42418
dimasm93 <software.dimas_m@icloud.com>
2024-12-19 15:26:21 +0700

    update script

--- src/ansible/ntnx-nkp/nkp-kommander-creation-airgap.sh
+++ src/ansible/ntnx-nkp/nkp-kommander-creation-airgap.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-export IMAGE='nkp-rocky-9.5-release-cis-1.30.5-20241204003513.qcow2'
-export SUBNET='mgnt.ntnx.ipam.local'
-export NKP_CLUSTER_NAME='nkp-kommander-hpoc'
-export CONTROLPLANE_VIP='10.10.20.5:6443'
-export METALLB_IP_RANGE='10.10.20.6-10.10.20.9'
-export NUTANIX_USER='admin'
-export NUTANIX_PASSWORD='Nutanix/4u@2024'
-export NUTANIX_ENDPOINT='https://10.10.20.31:9440'
-export CLUSTER='DELL-R730XD-AHV'
-export STORAGE_CONTAINER='default-storage'
-export SSH_PUBLIC_KEY='/home/user/.ssh/id_ed25519.pub'
-export REGISTRY_CACERT='/etc/docker/certs.d/airgap-0\:5000/registry-ca.crt'
-export REGISTRY_URL='https://airgap-0:5000'
-export REGISTRY_USERNAME='admin'
-export REGISTRY_PASSWORD='nutanix/4u'
+export IMAGE="nkp-rocky-9.5-release-cis-1.30.5-20241204003513.qcow2"
+export SUBNET="mgnt.ntnx.ipam.local"
+export NKP_CLUSTER_NAME="nkp-kommander-hpoc"
+export CONTROLPLANE_VIP="10.10.20.5:6443"
+export METALLB_IP_RANGE="10.10.20.6-10.10.20.9"
+export NUTANIX_USER="admin"
+export NUTANIX_PASSWORD="Nutanix/4u@2024"
+export NUTANIX_ENDPOINT="https://10.10.20.31:9440"
+export CLUSTER="DELL-R730XD-AHV"
+export STORAGE_CONTAINER="default-storage"
+export SSH_PUBLIC_KEY="/home/user/.ssh/id_ed25519.pub"
+export REGISTRY_CACERT="/etc/docker/certs.d/airgap-0\:5000/registry-ca.crt"
+export REGISTRY_URL="https://airgap-0:5000"
+export REGISTRY_USERNAME="admin"
+export REGISTRY_PASSWORD="nutanix/4u"
 
 nkp create cluster nutanix \
     --cluster-name=${CLUSTER_NAME} \
