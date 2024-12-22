334ad31cc838a8e26924cac91c3e3f5548bf72dd
dimasm93 <software.dimas_m@icloud.com>
2024-12-22 11:54:58 +0700

    update script deploy airgap

---
+++ src/ansible/ntnx-nkp/.deploy-registry.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+##############################################
+# Name        : Docker_registry_setup.sh
+# Author      : Dimas Maryanto
+# Version     : 1.0
+# Description : Script is used to configure registry with specified arguments
+##############################################
+
+mkdir -p certs auth && \
+docker run --rm --entrypoint htpasswd xmartlabs/htpasswd -Bbn "admin" "nutanix/4u" | tee auth/htpasswd && \
+openssl req -newkey rsa:4096 -nodes -sha256 -keyout certs/domain.key -x509 -days 365 -out certs/domain.crt -subj "/C=ID/ST=Indonesia/L=Jakarta/O=Nutanix/OU=IT/CN=*.nutanix.local"
+
+sudo mkdir -p /mnt/registry && \
+sudo docker run -d -p 5000:5000  --restart=always --name DockerRegistry -v /mnt/registry:/var/lib/registry -v `pwd`/auth:/auth -e "REGISTRY_AUTH=htpasswd" -e "REGISTRY_AUTH_HTPASSWD_REALM=Registry Realm" -e REGISTRY_AUTH_HTPASSWD_PATH=/auth/htpasswd -v `pwd`/certs:/certs -e REGISTRY_HTTP_TLS_CERTIFICATE=/certs/domain.crt -e REGISTRY_HTTP_TLS_KEY=/certs/domain.key registry:2
